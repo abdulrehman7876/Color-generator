e2267911b07b68b94a203de7b79fbfea0c3602a6
Abdul Rehman <145670921+abdulrehman7876@users.noreply.github.com>
2025-05-31 18:45:51 +0500

    Update color.js

--- color.js
+++ color.js
@@ -7,7 +7,6 @@ icon = document.querySelector(".icon")
 let hex;
 let dec;
 let rem;
-// let string;
 let myHexCode = "0123456789abcdef";
 let hexCode = "ffffff";
 let string = "";
@@ -16,54 +15,9 @@ changeColor = ()=>{
     string = "#";
     for (let i=0; i<6; i++){
         hexCode = myHexCode[Math.round(Math.random()*15)];
-        // console.log(hexCode);
         string = string + hexCode;
-        console.log(string);
     }
 
-
-    // hex = [];
-    // dec = Math.floor(Math.random() * Math.random() * 200000) * 2;
-    // string = "";
-    // for(let i = 0; i<6; i++) {
-    //     rem = dec % 16;
-    //     console.log(`rem: ${rem}`);
-    //     dec = Math.floor(dec / 16);
-    //     console.log(`dec: ${dec}`);
-    //     hex.push(rem);
-        
-    //     if(dec < 16){
-    //         hex.push(dec);
-    //         break;
-    //     }
-    // }
-    // console.log(hex);
-    
-    // hex.reverse();
-    
-    // hex.forEach((elem, ind, arr) => {
-    //     if(elem > 9 && elem < 16) {
-    //         if(elem === 10) arr[ind] = 'A';
-    //         if(elem === 11) arr[ind] = 'B';
-    //         if(elem === 12) arr[ind] = 'C';
-    //         if(elem === 13) arr[ind] = 'D';
-    //         if(elem === 14) arr[ind] = 'E';
-    //         if(elem === 15) arr[ind] = 'F';
-    //         }
-    //     }
-    // );
-    // string = hex.join("").padStart(6,Math.floor(Math.random() * 9));
-
-
-
-    // // for(let i = 0; string.length < 6; i++) {
-    // //     console.log(string.length);
-    // //     string = string + Math.floor(Math.random() * 10);
-    // // }
-
-
-
-    // console.log(string);
     box.style.backgroundColor = `${string}`;
     color.innerText = `${string}`;
     background.style.backgroundColor = `${string}`;
@@ -76,16 +30,12 @@ button.addEventListener('mouseover', ()=>{
 })
 button.addEventListener('click', ()=>{
     changeColor();
-    // box.style.backgroundColor = `#${string}`;
-    // console.log("hello");
 })
 icon.addEventListener('click', ()=>{
     navigator.clipboard.writeText(color.innerText);
     icon.style.color = "blue";
-    // icon.style.border = "1px solid black"
 })
 icon.addEventListener('mouseover', ()=>{
     icon.style.cursor = "pointer";
 })
-// box.style.backgroundColor = '#' + string;
 
